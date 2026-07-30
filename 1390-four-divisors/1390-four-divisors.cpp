91050566fefb107dd9f09f82d8938e72b632e311
Manish Kumar <manishkumar925657@gmail.com>
2026-07-30 17:07:56 +0530

    Time: 2907 ms (5.01%), Space: 16.2 MB (50.13%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-   int getDivisor(int n) {
-    int sum = 0;
-    int count = 0;
-
-    for (int div = 1; div <= n; div++) {
-        if (n % div == 0) {
-            count++;
-            sum += div;
-
-            if (count > 4)
-                return 0;
+    int getDivisor(int n){
+        int sum = n;
+        int div = n/2;
+        
+        int count =1;
+        while(div>0 && count  < 4){
+            if(n%div == 0){
+                sum += div;
+                count++;
+                div--;
+            }else{
+                div--;
+            }
         }
+        if(div == 0 && count == 4){
+            return sum;
+        }
+        return 0;
     }
-
-    return (count == 4) ? sum : 0;
-}
-
 
     int sumFourDivisors(vector<int>& nums) {
         int n = nums.size();
